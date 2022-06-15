ff709b4ea290413880282dc7355b91fe04a9b537
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-15 11:11:56 -0500

    Relationship classroom-student

--- student.rb
+++ student.rb
@@ -1,12 +1,32 @@
 require './person'
+require './classroom'
 
 class Student < Person
-  def initialize(name, age, classroom, parent_permission: true)
+  attr_accessor :classroom
+
+  def initialize(age, name, classroom = nil , parent_permission: true)
     @classroom = classroom
-    super(name, age, parent_permission)
+    super(age, name, parent_permission)
   end
 
   def play_hooky?
     "¯\(ツ)/¯"
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) if classroom.students.include?(self) == false
+  end
+
 end
+
+clase1A = Classroom.new('1A')
+
+juan_sotomayor = Student.new(18, 'Juan Sotomayor')
+pepe_perez = Student.new(17, 'Pepe Perez')
+
+
+# clase1A.add_student(juan_sotomayor)
+# clase1A.add_student(pepe_perez)
+
+p clase1A.students.map(&:name)
---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,2 @@
+clase1A.add_student(juan_sotomayor)
+# clase1A.add_student(pepe_perez)
