670b9c6a3727a9a4cbbbe5911dabe80ecc35fa2b
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-15 10:14:12 -0500

    Rental class created

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,7 @@
 class Classroom
-    attr_accessor :label
+  attr_accessor :label
 
-    def initialize(label)
-        @label = label
-    end
+  def initialize(label)
+    @label = label
+  end
 end
