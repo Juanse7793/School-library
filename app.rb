df81987f8b88be7b72f575c63389a801de624c3a
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-15 21:37:46 -0500

    create book method

--- app.rb
+++ app.rb
@@ -1,2 +1,29 @@
+require './student'
+require './teacher'
+require './book'
+require './rental'
+require './person'
+
 class App
+
+    attr_accessor :list_people, :list_books, :rentals
+
+    def initialize
+      @list_people = []
+      @list_books = []
+      @rentals = []
+    end
+
+    def create_book
+        puts 'Enter the name of the book:'
+        name = gets.chomp
+        puts 'Enter the author of the book:'
+        author = gets.chomp
+        puts 'Book created!'
+        puts "\n\n"
+        book = Book.new(name, author)
+        @list_books.push(book)
+    end
+
+
 end
