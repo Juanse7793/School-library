12dbc3138d669b388c3725f366bfdc115c86c525
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-15 21:40:15 -0500

    create rental method

--- app.rb
+++ app.rb
@@ -74,4 +74,24 @@ class App
         puts "\n\n"
       end
 
+    def create_rental
+        puts 'select a book from the following list by number (not ID):'
+        list_books.each_with_index do |book, index|
+          puts "[#{index}] Title: #{book.title}, Author: #{book.author}"
+        end
+        book_index = gets.chomp.to_i
+        puts 'select a person from the following list by number (not ID):'
+        list_people.each_with_index do |person, index|
+          puts "[#{index}] [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
+        end
+        person_index = gets.chomp.to_i
+        puts 'Enter the Date of rental:'
+        date = gets.chomp
+    
+        rental = Rental.new(date, list_books[book_index], list_people[person_index])
+        @rentals.push(rental)
+    
+        puts 'Rental created!'
+    end
+
 end
