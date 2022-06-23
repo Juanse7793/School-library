e0e771f0cb7e1a6cf9c009bc77a92c065985c450
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-22 22:58:30 -0500

    trimmer_decorator test

--- spec/decorators_spec.rb
+++ spec/decorators_spec.rb
@@ -9,7 +9,7 @@ describe BaseDecorator do
     end
   
 
-    it 'has a correct_name method' do
+    it 'has a correct_name? method' do
         expect(base_decorator.respond_to?(:correct_name?)).to eq(true)
     end
 
@@ -26,8 +26,29 @@ describe CapitalizeDecorator do
     it 'should be a capitalize decorator' do
         expect(capitalize_decorator).to be_a(CapitalizeDecorator)
     end
+
+    it 'has a correct_name? method' do
+        expect(capitalize_decorator.respond_to?(:correct_name?)).to eq(true)
+    end
             
     it 'correct_name equals the name of the person' do
         expect(capitalize_decorator.correct_name?).to eq(person.name.capitalize)
+    end
+end
+
+describe TrimmerDecorator do
+    let(:person) { Person.new('maximilianus', 22) }
+    let(:trimmer_decorator) { TrimmerDecorator.new(person) }
+
+    it 'should be a trimmer decorator' do
+        expect(trimmer_decorator).to be_a(TrimmerDecorator)
+    end
+
+    it 'has a correct_name? method' do
+        expect(trimmer_decorator.respond_to?(:correct_name?)).to eq(true)
+    end
+
+    it 'correct_name equals the name of the person' do
+        expect(trimmer_decorator.correct_name?).to eq(person.name.slice(0, 10))
     end
 end
